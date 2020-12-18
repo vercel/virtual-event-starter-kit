ff618339ab09c3c9a4d3d4f0ed68e2764db5e4ed
Lee Robinson <lrobinson2011@gmail.com>
2020-12-17 22:08:42 -0600

    Add support for Contentful CMS.

--- lib/cms-api.ts
+++ lib/cms-api.ts
@@ -13,132 +13,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { Job, Sponsor, Stage, Speaker } from '@lib/types';
 
-const API_URL = 'https://graphql.datocms.com/';
-const API_TOKEN = process.env.DATOCMS_READ_ONLY_API_TOKEN;
+import * as datoCmsApi from './cms-providers/dato';
+import * as contentfulApi from './cms-providers/contentful';
 
-async function fetchCmsAPI(query: string, { variables }: { variables?: Record<string, any> } = {}) {
-  const res = await fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${API_TOKEN}`
-    },
-    body: JSON.stringify({
-      query,
-      variables
-    })
-  });
+let cmsApi: any;
 
-  const json = await res.json();
-  if (json.errors) {
-    // eslint-disable-next-line no-console
-    console.error(json.errors);
-    throw new Error('Failed to fetch API');
-  }
-
-  return json.data;
+if (process.env.DATOCMS_READ_ONLY_API_TOKEN) {
+  cmsApi = datoCmsApi;
+} else if (process.env.CONTENTFUL_ACCESS_TOKEN) {
+  cmsApi = contentfulApi;
 }
 
-export async function getAllSpeakers() {
-  const data = await fetchCmsAPI(`
-    {
-      allSpeakers(first: 100) {
-        name
-        bio
-        title
-        slug
-        twitter
-        github
-        company
-        talk {
-          title
-          description
-        }
-        image {
-          url(imgixParams: {fm: jpg, fit: crop, w: 300, h: 400})
-        }
-        imageSquare: image {
-          url(imgixParams: {fm: jpg, fit: crop, w: 192, h: 192})
-        }
-      }
-    }
-  `);
-
-  return data.allSpeakers;
+export async function getAllSpeakers(): Promise<Speaker[]> {
+  return cmsApi.getAllSpeakers();
 }
 
-export async function getAllStages() {
-  const data = await fetchCmsAPI(`
-    {
-      allStages(first: 100, orderBy: order_ASC) {
-        name
-        slug
-        stream
-        discord
-        schedule {
-          title
-          start
-          end
-          speaker {
-            name
-            slug
-            image {
-              url(imgixParams: {fm: jpg, fit: crop, w: 120, h: 120})
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  return data.allStages;
+export async function getAllStages(): Promise<Stage[]> {
+  return cmsApi.getAllStages();
 }
 
-export async function getAllSponsors() {
-  const data = await fetchCmsAPI(`
-    {
-      allCompanies(first: 100, orderBy: tierRank_ASC) {
-        name
-        description
-        slug
-        website
-        callToAction
-        callToActionLink
-        discord
-        youtubeSlug
-        tier
-        links {
-          url
-          text
-        }
-        cardImage {
-          url(imgixParams: {fm: jpg, fit: crop})
-        }
-        logo {
-          url(imgixParams: {fm: jpg, fit: crop, w: 100, h: 100})
-        }
-      }
-    }
-  `);
-
-  return data.allCompanies;
+export async function getAllSponsors(): Promise<Sponsor[]> {
+  return cmsApi.getAllSponsors();
 }
 
-export async function getAllJobs() {
-  const data = await fetchCmsAPI(`
-    {
-      allJobs(first: 100, orderBy: rank_ASC) {
-        id
-        companyName
-        title
-        description
-        discord
-        link
-        rank
-      }
-    }
-  `);
-
-  return data.allJobs;
+export async function getAllJobs(): Promise<Job[]> {
+  return cmsApi.getAllJobs();
 }
---
+++ lib/cms-providers/dato.ts
@@ -0,0 +1,145 @@
+/**
+ * Copyright 2020 Vercel Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Job, Sponsor, Stage, Speaker } from '@lib/types';
+
+const API_URL = 'https://graphql.datocms.com/';
+const API_TOKEN = process.env.DATOCMS_READ_ONLY_API_TOKEN;
+
+async function fetchCmsAPI(query: string, { variables }: { variables?: Record<string, any> } = {}) {
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${API_TOKEN}`
+    },
+    body: JSON.stringify({
+      query,
+      variables
+    })
+  });
+
+  const json = await res.json();
+  if (json.errors) {
+    // eslint-disable-next-line no-console
+    console.error(json.errors);
+    throw new Error('Failed to fetch API');
+  }
+
+  return json.data;
+}
+
+export async function getAllSpeakers(): Promise<Speaker[]> {
+  const data = await fetchCmsAPI(`
+    {
+      allSpeakers(first: 100) {
+        name
+        bio
+        title
+        slug
+        twitter
+        github
+        company
+        talk {
+          title
+          description
+        }
+        image {
+          url(imgixParams: {fm: jpg, fit: crop, w: 300, h: 400})
+        }
+        imageSquare: image {
+          url(imgixParams: {fm: jpg, fit: crop, w: 192, h: 192})
+        }
+      }
+    }
+  `);
+
+  return data.allSpeakers;
+}
+
+export async function getAllStages(): Promise<Stage[]> {
+  const data = await fetchCmsAPI(`
+    {
+      allStages(first: 100, orderBy: order_ASC) {
+        name
+        slug
+        stream
+        discord
+        schedule {
+          title
+          start
+          end
+          speaker {
+            name
+            slug
+            image {
+              url(imgixParams: {fm: jpg, fit: crop, w: 120, h: 120})
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return data.allStages;
+}
+
+export async function getAllSponsors(): Promise<Sponsor[]> {
+  const data = await fetchCmsAPI(`
+    {
+      allCompanies(first: 100, orderBy: tierRank_ASC) {
+        name
+        description
+        slug
+        website
+        callToAction
+        callToActionLink
+        discord
+        youtubeSlug
+        tier
+        links {
+          url
+          text
+        }
+        cardImage {
+          url(imgixParams: {fm: jpg, fit: crop})
+        }
+        logo {
+          url(imgixParams: {fm: jpg, fit: crop, w: 100, h: 100})
+        }
+      }
+    }
+  `);
+
+  return data.allCompanies;
+}
+
+export async function getAllJobs(): Promise<Job[]> {
+  const data = await fetchCmsAPI(`
+    {
+      allJobs(first: 100, orderBy: rank_ASC) {
+        id
+        companyName
+        title
+        description
+        discord
+        link
+        rank
+      }
+    }
+  `);
+
+  return data.allJobs;
+}
