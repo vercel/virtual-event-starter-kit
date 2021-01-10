74a01664dee4beb5031a57b21cd216d691567c0d
Neeharika V <neeharules@gmail.com>
2021-01-09 16:00:10 -0800

    adjust indentations

--- components/stage-container.js
+++ components/stage-container.js
@@ -30,57 +30,57 @@ export default function StageContainer({ stage }) {
       <div className={styles.streamContainer}>
         {loginStatus === 'loggedIn' ? (
           <div className={cn(styles.stream, styleUtils.appear, styleUtils['appear-first'])}>
-          <iframe
-            allow="autoplay; picture-in-picture"
-            allowFullScreen
-            frameBorder="0"
-            src={`${stage.stream}?autoplay=1&mute=1`}
-            title={stage.name}
-            width="100%"
-          />
-          <div className={cn(styles.bottom, styleUtils.appear, styleUtils['appear-second'])}>
-            <div className={styles.messageContainer}>
-              <h2 className={styles.stageName}>{stage.name}</h2>
+            <iframe
+              allow="autoplay; picture-in-picture"
+              allowFullScreen
+              frameBorder="0"
+              src={`${stage.stream}?autoplay=1&mute=1`}
+              title={stage.name}
+              width="100%"
+            />
+            <div className={cn(styles.bottom, styleUtils.appear, styleUtils['appear-second'])}>
+              <div className={styles.messageContainer}>
+                <h2 className={styles.stageName}>{stage.name}</h2>
+              </div>
+              <a
+                href={stage.chat_link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.button}
+              >
+                <span>Join the chat on</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  viewBox="55 50 170 170"
+                >                
+                  <g>
+                    <g>
+                      <path fill="#E01E5A" d="M99.4,151.2c0,7.1-5.8,12.9-12.9,12.9c-7.1,0-12.9-5.8-12.9-12.9c0-7.1,5.8-12.9,12.9-12.9h12.9V151.2z"/>
+                      <path fill="#E01E5A" d="M105.9,151.2c0-7.1,5.8-12.9,12.9-12.9s12.9,5.8,12.9,12.9v32.3c0,7.1-5.8,12.9-12.9,12.9
+                        s-12.9-5.8-12.9-12.9V151.2z"/>
+                    </g>
+                    <g>
+                      <path fill="#36C5F0" d="M118.8,99.4c-7.1,0-12.9-5.8-12.9-12.9c0-7.1,5.8-12.9,12.9-12.9s12.9,5.8,12.9,12.9v12.9H118.8z"/>
+                      <path fill="#36C5F0" d="M118.8,105.9c7.1,0,12.9,5.8,12.9,12.9s-5.8,12.9-12.9,12.9H86.5c-7.1,0-12.9-5.8-12.9-12.9
+                        s5.8-12.9,12.9-12.9H118.8z"/>
+                    </g>
+                    <g>
+                      <path fill="#2EB67D" d="M170.6,118.8c0-7.1,5.8-12.9,12.9-12.9c7.1,0,12.9,5.8,12.9,12.9s-5.8,12.9-12.9,12.9h-12.9V118.8z"/>
+                      <path fill="#2EB67D" d="M164.1,118.8c0,7.1-5.8,12.9-12.9,12.9c-7.1,0-12.9-5.8-12.9-12.9V86.5c0-7.1,5.8-12.9,12.9-12.9
+                        c7.1,0,12.9,5.8,12.9,12.9V118.8z"/>
+                    </g>
+                    <g>
+                      <path fill="#ECB22E" d="M151.2,170.6c7.1,0,12.9,5.8,12.9,12.9c0,7.1-5.8,12.9-12.9,12.9c-7.1,0-12.9-5.8-12.9-12.9v-12.9H151.2z"/>
+                      <path fill="#ECB22E" d="M151.2,164.1c-7.1,0-12.9-5.8-12.9-12.9c0-7.1,5.8-12.9,12.9-12.9h32.3c7.1,0,12.9,5.8,12.9,12.9
+                        c0,7.1-5.8,12.9-12.9,12.9H151.2z"/>
+                    </g>
+                  </g>
+                </svg>
+              </a>
             </div>
-            <a
-              href={stage.chat_link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.button}
-            >
-              <span>Join the chat on</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="55 50 170 170"
-              >                
-                <g>
-                  <g>
-                    <path fill="#E01E5A" d="M99.4,151.2c0,7.1-5.8,12.9-12.9,12.9c-7.1,0-12.9-5.8-12.9-12.9c0-7.1,5.8-12.9,12.9-12.9h12.9V151.2z"/>
-                    <path fill="#E01E5A" d="M105.9,151.2c0-7.1,5.8-12.9,12.9-12.9s12.9,5.8,12.9,12.9v32.3c0,7.1-5.8,12.9-12.9,12.9
-                      s-12.9-5.8-12.9-12.9V151.2z"/>
-                  </g>
-                  <g>
-                    <path fill="#36C5F0" d="M118.8,99.4c-7.1,0-12.9-5.8-12.9-12.9c0-7.1,5.8-12.9,12.9-12.9s12.9,5.8,12.9,12.9v12.9H118.8z"/>
-                    <path fill="#36C5F0" d="M118.8,105.9c7.1,0,12.9,5.8,12.9,12.9s-5.8,12.9-12.9,12.9H86.5c-7.1,0-12.9-5.8-12.9-12.9
-                      s5.8-12.9,12.9-12.9H118.8z"/>
-                  </g>
-                  <g>
-                    <path fill="#2EB67D" d="M170.6,118.8c0-7.1,5.8-12.9,12.9-12.9c7.1,0,12.9,5.8,12.9,12.9s-5.8,12.9-12.9,12.9h-12.9V118.8z"/>
-                    <path fill="#2EB67D" d="M164.1,118.8c0,7.1-5.8,12.9-12.9,12.9c-7.1,0-12.9-5.8-12.9-12.9V86.5c0-7.1,5.8-12.9,12.9-12.9
-                      c7.1,0,12.9,5.8,12.9,12.9V118.8z"/>
-                  </g>
-                  <g>
-                    <path fill="#ECB22E" d="M151.2,170.6c7.1,0,12.9,5.8,12.9,12.9c0,7.1-5.8,12.9-12.9,12.9c-7.1,0-12.9-5.8-12.9-12.9v-12.9H151.2z"/>
-                    <path fill="#ECB22E" d="M151.2,164.1c-7.1,0-12.9-5.8-12.9-12.9c0-7.1,5.8-12.9,12.9-12.9h32.3c7.1,0,12.9,5.8,12.9,12.9
-                      c0,7.1-5.8,12.9-12.9,12.9H151.2z"/>
-                  </g>
-                </g>
-              </svg>
-            </a>
           </div>
-        </div>
         ) : loginStatus === 'loading' ? null : (
           <ConfEntry onRegister={() => mutate()} />
         )}
