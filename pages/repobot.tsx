4cf2db133c36d16e621a9d37ee394328e50f2e9c
adham-ta <69330652+adham-ta@users.noreply.github.com>
2021-03-06 11:35:41 +0300

    Create repobot.tsx

---
+++ pages/repobot.tsx
@@ -0,0 +1,25 @@
+طريقة الانضمام الى فريق عمل itt.bio العربي والانجليزي بكل بساطة تواصل معنا عبر الكومنت في هذا المقالة او عبر الايميل التالي : timytf@yandex.com
+
+مبدايا كانت طريقة الانظمام عن طريق رابط للانضمام الى الموقع لاكن تم إزالة هذي الخاصية وإضافة هذي الخاصية الى موقع الدعم الفني وهو ال Docs مبدأيا يمكنك الانضمام الى موقع itt.bio وايضا موقع. Docs وهو الدعم الفني
+
+ب
+
+بحاجة الى فريق عمل عاجلا
+
+Join the my docs
+
+لانضمام الىdocs الدعم الفني الخاص بنا عن طريق الرابط بسهولة
+
+الرابط: https://slite.com/organization/join-link/repobot/BwJimUXvnbJk8MQ2g97Fa9/default
+
+How to join the Arabic and English itt.bio team simply contact us via the comment in this article or via the following email: timytf@yandex.com
+
+Initially, the way to join was via a link to join the site, but this feature was removed and this feature was added to the technical support site, which is the Docs. Initially, you can join itt.bio and also a site. Docs which is technical support
+
+B
+
+Urgently need a team. Join the my docs
+
+To join our docs technical support via the link easily
+
+Link: https://slite.com/organization/join-link/repobot/BwJimUXvnbJk8MQ2g97Fa9/default
