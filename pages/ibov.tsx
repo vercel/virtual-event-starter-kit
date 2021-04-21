e89bb33ad3a952a2df17dc88a9a444940debf133
klingersf <60621086+klingersf@users.noreply.github.com>
2021-04-21 00:37:42 -0300

    Update ibov.tsx

--- pages/ibov.tsx
+++ pages/ibov.tsx
@@ -13,9 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+import {GetServerSideProps, GetStaticProps} from 'next';
 import IbovImage from '@components/ibov';
 
-export default function IbovOnlyPage() {
-  return <IbovImage />;
+
+export default function IbovOnlyPage({ data }) {
+  return <IbovImage ibovData={data}/>;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const uri = "https://statusinvest.com.br/acao/getaltabaixa?IndiceCode=ibovespa";
+    // const uri = "https://statusinvest.com.br/stock/getaltabaixa?IndiceCode=sp-500";
+    const res = await fetch(uri);
+    const data = await res.json();
+    // console.log(data);
+
+    return {
+        props: {
+            data
+        } // will be passed to the page component as props
+    };
+};
