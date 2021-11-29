e6dc8e4e4d380c80d5de504ab531c653d43cfe66
Manny Colon <colonmanuel7@gmail.com>
2021-11-29 13:52:48 -0500

    lib/constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'opencomnponentshackathon.netlify.app';
+export const SITE_URL = 'https://open-components-hackathon.netlify.app/';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'unfoldingWord';
 export const BRAND_NAME = 'Open Components';
