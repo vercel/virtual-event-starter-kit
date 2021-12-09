1c5732c2a9e1f3b8f56dc351ea61d88de5efe40c
Manny Colon <colonmanuel7@gmail.com>
2021-12-09 11:15:23 -0500

    Fixed logo

--- components/icons/icon-logo.tsx
+++ components/icons/icon-logo.tsx
@@ -27,8 +27,8 @@ export default function IconLogo({
       src='/uw-logo.svg'
       className={''}
       loading="eager"
-      height={72}
-      width={100}
+      height={120}
+      width={180}
     />
   );
 }
