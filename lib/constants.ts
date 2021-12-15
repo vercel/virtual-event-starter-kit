701b74b8076b87410c149fcfaa328af1d5de53e2
Manny Colon <colonmanuel7@gmail.com>
2021-12-15 13:41:35 -0500

    og:description

--- lib/constants.ts
+++ lib/constants.ts
@@ -20,7 +20,7 @@ export const TWITTER_USER_NAME = 'unfoldingWord';
 export const BRAND_NAME = 'Open Components';
 export const SITE_NAME_MULTILINE = ['Open', 'Components', 'Hackathon'];
 export const SITE_NAME = 'Open Components Hackathon';
-export const META_DESCRIPTION = 'Open Components Hackathon Event.';
+export const META_DESCRIPTION = 'Join our Open Components Hackathon Event.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
 export const DATE = 'February 28th - March 4th, 2022';
