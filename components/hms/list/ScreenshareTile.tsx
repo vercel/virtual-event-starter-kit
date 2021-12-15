3af754a0dd1d467a65b66c3aad7916b9896aa680
Deepankar Bhade <deep14bhade@gmail.com>
2021-12-15 16:23:17 +0530

    feat: full screen & sharer's name

--- components/hms/list/ScreenshareTile.tsx
+++ components/hms/list/ScreenshareTile.tsx
@@ -4,8 +4,10 @@ import {
   selectPeerScreenSharing
 } from '@100mslive/hms-video-store';
 import { useHMSActions, useHMSStore } from '@100mslive/react-sdk';
+import { ShareScreenIcon } from '@100mslive/react-icons';
 import React from 'react';
 import s from './index.module.css';
+import IconFitScreen from '@components/icons/icon-fit-screen';
 
 const ScreenshareTile = () => {
   const screenSharePeer = useHMSStore(selectPeerScreenSharing);
@@ -32,6 +34,12 @@ const ScreenshareTile = () => {
       console.log('Error: ', error);
     }
   };
+  const fullScreen = () => {
+    if (typeof window !== 'undefined') {
+      const element = document.getElementById('screen-share-video');
+      element?.requestFullscreen();
+    }
+  };
   return (
     <>
       {isLocalScreenShared ? (
@@ -42,12 +50,22 @@ const ScreenshareTile = () => {
           </button>
         </div>
       ) : (
-        <video
-          style={{ height: 'calc(100vh - 3 * var(--header-height))' }}
-          ref={videoRef}
-          autoPlay
-          muted
-        ></video>
+        <div className="relative" style={{ height: 'calc(100vh - 3.2 * var(--header-height))' }}>
+          <div
+            className="absolute flex items-center bottom-0 right-0 text-sm p-2 rounded-tl-lg"
+            style={{ backgroundColor: 'rgba(0,0,0,0.8)' }}
+          >
+            <ShareScreenIcon className="mr-2" /> Viewing {screenSharePeer?.name}’s Screen
+          </div>
+          <button
+            onClick={fullScreen}
+            style={{ backgroundColor: 'rgba(0,0,0,0.8)' }}
+            className="absolute top-0 right-0 display items-center  justify-center rounded-bl-lg cursor-pointer z-10"
+          >
+            <IconFitScreen />
+          </button>
+          <video id="screen-share-video" className="h-full" ref={videoRef} autoPlay muted></video>
+        </div>
       )}
     </>
   );
