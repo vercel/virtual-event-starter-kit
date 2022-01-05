6a1386c6b86bf85eaf2f4fa78c302685a95b3662
Deepankar Bhade <deep14bhade@gmail.com>
2022-01-05 10:47:16 +0530

    chore: remove need for email

--- components/stage-container.tsx
+++ components/stage-container.tsx
@@ -43,7 +43,7 @@ export default function StageContainer({ stage, allStages }: Props) {
   return (
     <div className={styles.container}>
       <div className={`${styles.streamContainer} ${isConnected ? '' : styles.streamYt}`}>
-        {loginStatus === 'loggedIn' ? (
+        {true ? (
           !stage.isLive ? (
             <div className={cn(styles.stream, styleUtils.appear, styleUtils['appear-first'])}>
               <iframe
