afff5b72ecae585a492469d16f3852350ece5cdb
thorwebdev <thor@supabase.io>
2022-07-14 18:21:06 +0800

    chore: update README

--- lib/db-providers/supabase/schema.sql
+++ lib/db-providers/supabase/schema.sql
@@ -6,9 +6,11 @@ create table users (
   username text unique,
   "createdAt" timestamp with time zone default timezone('utc'::text, now()) not null
 );
+alter table users enable row level security;
 
 create table github_users (
   id uuid primary key default uuid_generate_v4(),
   "createdAt" timestamp with time zone default timezone('utc'::text, now()) not null,
   "userData" jsonb
 );
+alter table github_users enable row level security;
