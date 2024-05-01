a6c30b63dd74123be59acf0247e78bc16f187457
Lee Robinson <lrobinson2011@gmail.com>
2024-04-30 22:54:30 -0500

    Codemod

--- components/sponsors-grid.tsx
+++ components/sponsors-grid.tsx
@@ -22,37 +22,37 @@ import styles from './sponsors-grid.module.css';
 
 function SponsorCard({ sponsor }: { sponsor: Sponsor }) {
   return (
-    <Link key={sponsor.name} href={`/expo/${sponsor.slug}`}>
-      <a
-        role="button"
-        tabIndex={0}
-        className={cn(styles.card, {
-          [styles.diamond]: sponsor.tier === 'diamond',
-          [styles.gold]: sponsor.tier === 'gold'
-        })}
-      >
-        <div className={styles.imageWrapper}>
-          <Image
-            alt={sponsor.name}
-            src={sponsor.cardImage.url}
-            className={cn(styles.image, {
-              [styles.silver]: sponsor.tier === 'silver'
-            })}
-            loading="lazy"
-            title={sponsor.name}
-            width={900}
-            height={500}
-          />
-        </div>
-        {sponsor.tier !== 'silver' && (
-          <div className={styles.cardBody}>
-            <div>
-              <h2 className={styles.name}>{sponsor.name}</h2>
-              <p className={styles.description}>{sponsor.description}</p>
-            </div>
+    <Link
+      key={sponsor.name}
+      href={`/expo/${sponsor.slug}`}
+      role="button"
+      tabIndex={0}
+      className={cn(styles.card, {
+        [styles.diamond]: sponsor.tier === 'diamond',
+        [styles.gold]: sponsor.tier === 'gold'
+      })}
+    >
+      <div className={styles.imageWrapper}>
+        <Image
+          alt={sponsor.name}
+          src={sponsor.cardImage.url}
+          className={cn(styles.image, {
+            [styles.silver]: sponsor.tier === 'silver'
+          })}
+          loading="lazy"
+          title={sponsor.name}
+          width={900}
+          height={500}
+        />
+      </div>
+      {sponsor.tier !== 'silver' && (
+        <div className={styles.cardBody}>
+          <div>
+            <h2 className={styles.name}>{sponsor.name}</h2>
+            <p className={styles.description}>{sponsor.description}</p>
           </div>
-        )}
-      </a>
+        </div>
+      )}
     </Link>
   );
 }
